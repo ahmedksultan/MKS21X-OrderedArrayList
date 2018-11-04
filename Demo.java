89a9656110fb85b7131acc6987aebf45d9454736
asultan00 <asultan00@stuy.edu>
2018-11-04 07:29:00 -0500

    edited driver

--- Demo.java
+++ Demo.java
@@ -1,13 +1,20 @@
 public class Demo {
      public static void main(String[] args) {
+          System.out.println("\nCreating new NoNullArrayList...Complete!");
           NoNullArrayList<String> smashbros = new NoNullArrayList<>();
+          System.out.println("Adding items to new list...");
           smashbros.add("mario");
           smashbros.add("luigi");
           smashbros.add("ness");
           smashbros.add("pikachu");
+          System.out.println("Adding item to index one of list...");
           smashbros.add(1, "incineroar");
-          System.out.println(smashbros); // [mario, incineroar, luigi, ness, pikachu]
+          System.out.println("...Complete! Printing list...\n\texpected output: [mario, incineroar, luigi, ness, pikachu]");
+          System.out.println("\tprinted  output: " + smashbros); // [mario, incineroar, luigi, ness, pikachu]
+          System.out.println("\nReplacing item at index one of list...");
           smashbros.set(1, "king k rool");
-          System.out.println(smashbros);// [mario, king k rool, luigi, ness, pikachu]
+          System.out.println("...Complete! Printing list...\n\texpected output: [mario, king k rool, luigi, ness, pikachu]");
+          System.out.println("\tprinted  output: " + smashbros);// [mario, king k rool, luigi, ness, pikachu]
+          System.out.println();
      }
 }
