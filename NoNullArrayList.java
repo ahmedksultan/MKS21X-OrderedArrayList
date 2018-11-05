fe4fd5e6d483d15ff86f92fc9304a2c5e8eb21a5
asultan00 <asultan00@stuy.edu>
2018-11-05 09:46:47 -0500

    edited code

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -5,8 +5,8 @@ public class NoNullArrayList<T> extends ArrayList<T> {
      public NoNullArrayList() {
           super(0);
      }
-     public NoNullArrayList(int size) {
-          super(size);
+     public NoNullArrayList(int startingCapacity) {
+          super(startingCapacity);
      }
      public boolean add(T element) {
           if (element == null) {
--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -2,13 +2,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
-     /* This is a NoNullArrayList that maintains all elements in a sorted order upon insertion.
-
-    Override both add methods -> find the correct index where the new element should be added,
-                                 then insert the new element into that position
-                                 (hint: 1)compareTo 2) helper method)
-
-    Override set(index,value) -> remove the element at index, then add the new value.
-
-    Only have 2 constructors:  default + startingCapacity. */
+     public OrderedArrayList() {
+          super(0);
+     }
+     public OrderedArrayList(int startingCapacity) {
+          super(startingCapacity);
+     }
+     public boolean add(T element) {
+          if (element == null) {
+               throw new IllegalArgumentException("**ERROR - cannot add null");
+          }
+          for (int i = 0; i < size(); i++) {
+               if (element.compareTo(get(i)) <= 0) {
+                    super.add(i, element);
+                    return true;
+               }
+          }
+          return super.add(element);
+     }
+     public void add(int index, T element) {
+          if (element == null) {
+               throw new IllegalArgumentException("**ERROR - cannot add null");
+          }
+          add(element);
+     }
+     public T set(int index, T element) {
+          if (element == null) {
+               throw new IllegalArgumentException("**ERROR - cannot set null");
+          }
+          T removed = remove(index);
+          add(element);
+          return removed;
+     }
 }
